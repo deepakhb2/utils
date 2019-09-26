5042f1f77c4329aaa6194a1f6e4b8184fcf05cb5
Deepak HB <deepakhb2@gmail.com>
2019-09-26 16:40:06 +0100

    Add django/wagtail alias commands.

--- bash/alias/bashrc/projects.bash
+++ bash/alias/bashrc/projects.bash
@@ -14,9 +14,21 @@ done
 ## Rails Alaises
 alias rc='rails c'
 alias rs='rails s'
+alias rg='rails g'
 
 ## npm
 alias ns='npm start'
 alias ni='npm install'
 alias nb='npm run build'
 alias nii='npm -i install'
+
+## Django - Python - Wagtail
+alias py='python'
+alias pyvenv='python -m venv'                    # pyvenv mysite/env
+alias pymm='python manage.py migrate'
+alias pymcsu='python manage.py createsuperuser'
+alias pymrs='python manage.py runserver'
+alias pipi='pip install'
+alias pipir='pip install -r'
+alias pipirr='pip install -r requirements.txt'
+alias wags='wagtail start'                       # wags mysite
