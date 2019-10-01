8bd26b29350d7f59208e17b3e1085db2186c105a
Deepak HB <deepakhb2@gmail.com>
2019-10-01 14:58:55 +0100

    Add docker alias for stop.

--- bash/alias/bashrc/docker.bash
+++ bash/alias/bashrc/docker.bash
@@ -2,6 +2,7 @@ alias dr='docker'
 alias drc='docker-compose'
 alias drcb='docker-compose build'
 alias drcu='docker-compose up'
+alias drcd='docker-compose down'
 alias drps='docker ps'
 alias drei='docker exec -it'                       # ssh login: docker exec -it <container name or id> /bin/bash
 alias drcps='docker-compose ps'
