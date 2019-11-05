7c60fd54daa606da2673b2bf80e152e40fc30d01
Deepak HB <deepakhb2@gmail.com>
2019-11-05 15:18:45 +0000

    Add comments and ssh to docker image alias command.

--- bash/alias/bashrc/docker.bash
+++ bash/alias/bashrc/docker.bash
@@ -5,20 +5,20 @@ alias drcu='docker-compose up'
 alias drcd='docker-compose down'
 alias drps='docker ps'
 alias drcr='docker-compose run'
-alias dre-i='docker exec -it'                       # ssh login: docker exec -it <container name or id> /bin/bash
+alias dre-i='docker exec -it'                         # ssh login to container: docker exec -it <container name or id> /bin/bash
+alias drr-i='docker run -it'                          # ssh login to image: docker run -it <image name or id> /bin/bash
 alias drcps='docker-compose ps'
 alias drcb-nc='docker-compose build --no-cache'
-alias drcr-r='docker-compose run --rm'               # web-prod python manage.py test report
+alias drcr-r='docker-compose run --rm'                # web-prod python manage.py test report
 
 
 alias dri='docker images'
-alias dri-none='docker images | grep "^<none>"'
+alias dri-none='docker images | grep "^<none>"'       # List all the images by name <none>
 alias dri-q='docker images -q'
-alias drrmi='docker rmi'
+alias drrmi='docker rmi'                              # eg. drrmi <image name or id>
 alias drrmi-f='docker rmi -f'
-alias drrmi-none='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
-alias drrmi-f-none='docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")'
-
+alias drrmi-none='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'        # Remove all the images with name <none>
+alias drrmi-f-none='docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")'   # Force remove all the images with name <none>
 
 #docker volume ls -qf dangling=true
 #docker volume rm $(docker volume ls -qf dangling=true)
