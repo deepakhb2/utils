9faf0e537004bd3c3ab147e02c1217264f0c3c49
Deepak HB <deepakhb2@gmail.com>
2020-02-20 22:31:01 +0000

    Add alias to start pg service with scpt.

--- bash/alias/bashrc/services.bash
+++ bash/alias/bashrc/services.bash
@@ -1,5 +1,7 @@
 alias start-elastic-search="$services/elasticsearch-7.6.0/bin/elasticsearch"
 alias start-redis-server="redis-server"
 alias start-mongod="mongod --dbpath ~/data/db"
+alias start-pg_ctl="pg_ctl -D /usr/local/var/postgres start"
 
 alias launch-services="osascript $utils/scripts/launch-services.scpt"
+alias launch-elastic-redis-pg="osascript $utils/scripts/launch-elastic-redis-pg.scpt"
