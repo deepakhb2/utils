fd1282c4663c6aa0ba3a84d7faa229087baad53a
Deepak HB <deepakhb2@gmail.com>
2020-02-20 22:12:50 +0000

    Add alias for mongodb, redis and elastic search. Add scpt scripts to start services.

--- bash/alias/bashrc/elastic.bash
+++
@@ -1,1 +0,0 @@
-alias elasticsearch="/Users/deepak/Projects/services/elasticsearch-7.6.0/bin/elasticsearch"
---
+++ bash/alias/bashrc/services.bash
@@ -0,0 +1,5 @@
+alias start-elastic-search="$services/elasticsearch-7.6.0/bin/elasticsearch"
+alias start-redis-server="redis-server"
+alias start-mongod="mongod --dbpath ~/data/db"
+
+alias launch-services="osascript $utils/scripts/launch-services.scpt"
