62d3b8f4ebc871e2fcce6a399d1d3f2e6f5860dc
Deepak HB <deepakhb2@gmail.com>
2020-08-11 02:06:36 +0100

    Add patch git command aliases.
    Add alia commands for tmux.

---
+++ bash/alias/bashrc/tmux.bash
@@ -0,0 +1,7 @@
+alias tx='tmux'
+alias txn='tmux new'
+alias txn-s='tmux new -s'             # Create new tmux session eg. txn-s <session>
+alias txls='tmux ls'
+alias txa-s='tmux attach-session'     # Attach to last session
+alias txa-s-t='tmux attach-session -t'  # Attach to a session eg. txa-s-t <session>
+alias txk-s-t='tmux kill-session -t'
