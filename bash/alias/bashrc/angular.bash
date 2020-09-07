0fb76ac830f4b7b691ad9f58716dc11434838123
Deepak HB <deepakhb2@gmail.com>
2020-09-07 10:29:55 +0100

    Add alias commands for angular and firebase.

---
+++ bash/alias/bashrc/angular.bash
@@ -0,0 +1,6 @@
+alias ngn='ng new'                #ng new my-workspace --create-application=false
+alias ngb='ng build'
+alias ngt='ng test'
+alias ngl='ng lint'
+alias nggl='ng generate library'  #ng generate library my-lib
+alias
