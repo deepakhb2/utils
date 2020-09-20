2891c1b9c7d48243e693b2ec182a46186df4036e
Deepak HB <deepakhb2@gmail.com>
2020-09-20 21:45:06 +0100

    Add alias commands for npmrc.

--- bash/alias/bashrc/npm.bash
+++ bash/alias/bashrc/npm.bash
@@ -2,3 +2,8 @@ alias ns='npm start'
 alias ni='npm install'
 alias nb='npm run build'
 alias nii='npm -i install'
+alias ncser='npm config set registry'
+
+alias nrc='npmrc'
+alias nrc-c='npmrc -c'
+
