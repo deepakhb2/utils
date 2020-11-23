8305d8e7cab1f1b6f57415b81557afa73eb33720
Deepak HB <deepakhb2@gmail.com>
2020-11-23 00:24:09 +0000

    Fix the ctags command. g

--- bash/alias/bashrc/ctags.bash
+++ bash/alias/bashrc/ctags.bash
@@ -1,3 +1,3 @@
 alias ctags-R="ctags -R"            # ctags-R --exclude=.git --exclude=log .
 alias ctags-R-typescript="ctags -R --language-force=typescript --exclude=node_modules --exclude=dist --exclude=documentation"
-alias ctags-R-ruby="ctags -R --languages=ruby --exclude=.git --exclude=log --exclude=tmp . $(bundle list --paths)"
+alias ctags-R-ruby="ctags -R --languages=ruby --exclude=.git --exclude=log --exclude=tmp"   #  * $(bundle list --paths)
