1d9dc8708e86bc0e754ab976c40fb2e954b51f9c
Deepak HB <deepakhb2@gmail.com>
2020-12-04 00:39:18 +0000

    Make tmux optional with more alias commands.

--- bash/alias/bashrc/projects.bash
+++ bash/alias/bashrc/projects.bash
@@ -6,16 +6,22 @@ do
     dir=${temppath##*/}
     bname=$(basename $dir)
     bname=$(echo $bname | sed "s/\./-/g")
-    alias ${dir}="cd $temppath;tmux attach -t $bname; tmux new -s $bname"
-    alias ${dir}-code="cd $temppath;code .;tmux attach -t $bname; tmux new -s $bname"
-    alias ${dir}-mvim="cd $temppath;mvim;tmux attach -t $bname; tmux new -s $bname"
+    alias ${dir}="cd $temppath"
+    alias ${dir}-code="cd $temppath;code ."
+    alias ${dir}-mvim="cd $temppath;mvim"
+    alias ${dir}-tmux="cd $temppath;tmux attach -t $bname; tmux new -s $bname"
+    alias ${dir}-tmux-code="cd $temppath;code .;tmux attach -t $bname; tmux new -s $bname"
+    alias ${dir}-tumx-mvim="cd $temppath;mvim;tmux attach -t $bname; tmux new -s $bname"
   done
 done
 
 for dirPath in $dirPaths
 do
   bname=$(basename $dirPath)
-  alias ${bname}="cd $dirPath;tmux attach -t $bname;tmux new -s $bname"
-  alias ${bname}-code="cd $dirPath;code .;tmux attach -t $bname;tmux new -s $bname"
-  alias ${bname}-mvim="cd $dirPath;mvim;tmux attach -t $bname;tmux new -s $bname"
+  alias ${bname}="cd $dirPath"
+  alias ${bname}-code="cd $dirPath;code ."
+  alias ${bname}-mvim="cd $dirPath;mvim"
+  alias ${bname}-tmux="cd $dirPath;tmux attach -t $bname;tmux new -s $bname"
+  alias ${bname}-tmux-code="cd $dirPath;code .;tmux attach -t $bname;tmux new -s $bname"
+  alias ${bname}-tmux-mvim="cd $dirPath;mvim;tmux attach -t $bname;tmux new -s $bname"
 done
