c3648280aa4fc0a9b55ab938246cfde6c07a58a0
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2022-01-28 09:50:32 +0000

    Add linux alias commands to get os information.

---
+++ bash/alias/bashrc/linux.bash
@@ -0,0 +1,3 @@
+alias cmdbitinfo='getconf LONG_BIT'         # This command check if the OS (kernel) is 32 bit or 64 bit.
+alias cmduname='uname'
+alias cmduname-a='uname -a'                 # This command prints system information including kernel version and whether kernel (os) is 32 bit or 64 bit.
