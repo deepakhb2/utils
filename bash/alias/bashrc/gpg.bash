c504dee3d082e330c57758b3d6366e790e0b7ff8
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2022-02-06 23:00:56 +0000

    Add aliases for gpg commands.

---
+++ bash/alias/bashrc/gpg.bash
@@ -0,0 +1,14 @@
+alias gpg--l-k='gpg --list-keys'
+alias gpg--g-k='gpg --gen-key'
+alias gpg--f-g-k='gpg --full-gen-key'
+alias gpg--l-s-k='gpg --list-secret-keys'
+alias gpg--r-k='gpg --refresh-keys'
+alias gpg--d-k='gpg --delete-key'
+alias gpg--d-s-k='gpg --delete-secret-key'
+alias gpg--a--e='gpg --armor --export'        # gpg--a--e <key-id>
+alias gpg--e='gpg --edit'                     # gpg--e <key-id>
+alias gpg--ks='gpg --keyserver'               # gpg--ks <server-url> --recv-keys <key-id>
+alias gpg--fp='gpg --fingerprint'
+alias gpg-e-u='gpg -e -u'                     # gpg-e-u "Sender User Name" -r "Receiver User Name" somefile'
+alias gpg-d='gpg -d'                          # gpg-d <encryptedfile>
+alias gpg--g-r='gpg --gen-revoke'
