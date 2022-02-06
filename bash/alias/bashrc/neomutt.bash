7c94039b2099b85db2785ee87b7ee8a9c23e4e07
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2022-02-06 16:46:23 +0000

    Add alias commands for neomutt.

---
+++ bash/alias/bashrc/neomutt.bash
@@ -0,0 +1,8 @@
+alias nmutt='neomutt'
+
+alias mwa='mw add'            # mw add -- add a new email account
+alias mwl='mw ls'             # mw ls -- list existing accounts
+alias mwpass='mw pass'        # mw pass -- revise an account's password
+alias mwd='mw delete'         # mw delete -- delete an added account
+alias mws='mw sync'           # mw sync -- sync all mail accounts and update notmuch database
+alias mwpurge= 'mw purge'     # mw purge -- delete all accounts and settings
