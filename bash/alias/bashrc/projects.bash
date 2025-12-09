afde9cc38610382951e7f62e357be1297184444f
Deepak Hagadur Bheemaraju <deepak@Deepaks-MacBook-Pro.local>
2025-12-09 23:23:29 +0000

    refactor(aliases): replace mvim with nvim in project aliases
    Update project and directory alias definitions to use Neovim (nvim)
    instead of MacVim (mvim) for navigation and tmux integration. This
    modernizes the workflow and aligns with current editor preferences.

--- bash/alias/bashrc/projects.bash
+++ bash/alias/bashrc/projects.bash
@@ -1,27 +1,24 @@
 alias proj-utils="cd $utils"
-for project in $projects
-do
-  for temppath in $(find $project ! -path $project -maxdepth 1 -type d)
-  do
+for project in $projects; do
+  for temppath in $(find $project ! -path $project -maxdepth 1 -type d); do
     dir=${temppath##*/}
     bname=$(basename $dir)
     bname=$(echo $bname | sed "s/\./-/g")
     alias ${dir}="cd $temppath"
     alias ${dir}-code="cd $temppath;code ."
-    alias ${dir}-mvim="cd $temppath;mvim"
+    alias ${dir}-nvim="cd $temppath;nvim"
     alias ${dir}-tmux="cd $temppath;tmux attach -t $bname; tmux new -s $bname"
     alias ${dir}-tmux-code="cd $temppath;code .;tmux attach -t $bname; tmux new -s $bname"
-    alias ${dir}-tmux-mvim="cd $temppath;mvim;tmux attach -t $bname; tmux new -s $bname"
+    alias ${dir}-tmux-nvim="cd $temppath;tmux attach -t $bname; tmux new -s $bname; nvim"
   done
 done
 
-for dirPath in $dirPaths
-do
+for dirPath in $dirPaths; do
   bname=$(basename $dirPath)
   alias ${bname}="cd $dirPath"
   alias ${bname}-code="cd $dirPath;code ."
-  alias ${bname}-mvim="cd $dirPath;mvim"
+  alias ${bname}-nvim="cd $dirPath;nvim"
   alias ${bname}-tmux="cd $dirPath;tmux attach -t $bname;tmux new -s $bname"
   alias ${bname}-tmux-code="cd $dirPath;code .;tmux attach -t $bname;tmux new -s $bname"
-  alias ${bname}-tmux-mvim="cd $dirPath;mvim;tmux attach -t $bname;tmux new -s $bname"
+  alias ${bname}-tmux-nvim="cd $dirPath;tmux attach -t $bname;tmux new -s $bname;nvim"
 done
