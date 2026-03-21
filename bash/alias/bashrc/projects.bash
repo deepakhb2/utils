e7a5cd6ee40048dd7cd99b010ac33aadf115dfee
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2026-03-21 19:56:56 +0000

    fix(aliases): use subshells for tmux aliases to prevent directory change in current session

--- bash/alias/bashrc/projects.bash
+++ bash/alias/bashrc/projects.bash
@@ -4,23 +4,24 @@ for project in $projects; do
     dir=${temppath##*/}
     bname=$(basename $dir)
     bname=$(echo $bname | sed "s/\./-/g")
-    alias ${dir}="cd $temppath"
-    alias ${dir}-code="cd $temppath;code ."
+    alias ${dir}="cd \"$temppath\""
+    alias ${dir}-code="cd \"$temppath\";code ."
     # alias ${dir}-nvim="cd $temppath;nvim"
-    alias ${dir}-tmux="cd $temppath;tmux attach -t $bname; tmux new -s $bname"
-    alias ${dir}-tmux-code="cd $temppath;code .;tmux attach -t $bname; tmux new -s $bname"
-    alias ${dir}-tmux-nvim="cd $temppath;tmux attach -t $bname; tmux new -s $bname 'nvim'"
-    alias ${dir}-nvim="cd $temppath;tmux attach -t $bname; tmux new -s $bname 'nvim'"
+    alias ${dir}-tmux="(cd \"$temppath\";tx-goto-session \"$bname\")"
+    alias ${dir}-tmux-code="(cd \"$temppath\";code .;tx-goto-session \"$bname\")"
+    alias ${dir}-tmux-nvim="(cd \"$temppath\";tx-goto-session \"$bname\" 'nvim')"
+    alias ${dir}-nvim="(cd \"$temppath\";tx-goto-session \"$bname\" 'nvim')"
   done
 done
 
 for dirPath in $dirPaths; do
   bname=$(basename $dirPath)
-  alias ${bname}="cd $dirPath"
-  alias ${bname}-code="cd $dirPath;code ."
+  bname=$(echo $bname | sed "s/\./-/g")
+  alias ${bname}="cd \"$dirPath\""
+  alias ${bname}-code="cd \"$dirPath\";code ."
   # alias ${bname}-nvim="cd $dirPath;nvim"
-  alias ${bname}-tmux="cd $dirPath;tmux attach -t $bname;tmux new -s $bname"
-  alias ${bname}-tmux-code="cd $dirPath;code .;tmux attach -t $bname;tmux new -s $bname"
-  alias ${bname}-tmux-nvim="cd $dirPath;tmux attach -t $bname;tmux new -s $bname 'nvim'"
-  alias ${bname}-nvim="cd $dirPath;tmux attach -t $bname;tmux new -s $bname 'nvim'"
+  alias ${bname}-tmux="(cd \"$dirPath\";tx-goto-session \"$bname\")"
+  alias ${bname}-tmux-code="(cd \"$dirPath\";code .;tx-goto-session \"$bname\")"
+  alias ${bname}-tmux-nvim="(cd \"$dirPath\";tx-goto-session \"$bname\" 'nvim')"
+  alias ${bname}-nvim="(cd \"$dirPath\";tx-goto-session \"$bname\" 'nvim')"
 done
--- bash/alias/bashrc/tmux.bash
+++ bash/alias/bashrc/tmux.bash
@@ -6,3 +6,26 @@ alias txls='tmux ls'
 alias txn-s='tmux new -s'             # Create new tmux session eg. txn-s <session>
 alias txn='tmux new'
 alias txr-n-t='tmux rename-session -t'  # Rename session eg. txr-n-t <old-name> <new-name>
+
+function tx-goto-session() {
+  local session="$1"
+  shift
+  local dir="$(pwd)"
+
+  if [ -n "$TMUX" ]; then
+    if ! tmux has-session -t "$session" 2>/dev/null; then
+      # Create session in background, starting in current dir
+      tmux new-session -s "$session" -c "$dir" -d "$@"
+    fi
+    # Switch to the session
+    tmux switch-client -t "$session"
+    
+    # If a command was passed and session already existed, 
+    # the user probably wants that command running.
+    # We can send the command to the session if it's just at a prompt, 
+    # but that's risky. Instead, let's just ensure we switch.
+  else
+    # Not in tmux: attach or create
+    tmux attach-session -t "$session" || tmux new-session -s "$session" -c "$dir" "$@"
+  fi
+}
